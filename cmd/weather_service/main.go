fa2508b9e9ed9983ea85f2243d5ceff2a915435e
Federico Paredes <paredfed@gmail.com>
2024-10-02 13:33:56 -0600

    skeleton

---
+++ cmd/weather_service/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+	service "weather-service/service"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "time for connections to finish to shutdown gracefully")
+	flag.Parse()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/weather", service.HandleGetWeather).Methods("GET")
+
+	srv := &http.Server{
+		Addr:         "0.0.0.0:8080",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// Will graceful shutdown on SIGINT
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+	log.Println("shutting down")
+	os.Exit(0)
+}
---
+++ service/service.go
@@ -0,0 +1,10 @@
+package service
+
+import (
+	"net/http"
+)
+
+// HandleGetWeather will receive lat and long within the http and return weather of that location
+func HandleGetWeather(w http.ResponseWriter, r *http.Request) {
+	//do things
+}
